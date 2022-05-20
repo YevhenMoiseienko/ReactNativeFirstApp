ffe0896013aa7519f9f273ccf3651a1312f488af
Yevhen Moiseienko <mojseenko2011@gmail.com>
2022-05-20 13:20:49 +0300

    Add delete icon for TodoItem

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,17 +1,24 @@
-import {StyleSheet, Text, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
 
 export const TodoItem = ({item, onDeleteTodo}) => {
 
 
     return (
-        <TouchableOpacity onPress={() => onDeleteTodo(item.key)}>
-            <Text style={styles.item}>{item.text}</Text>
+        <TouchableOpacity>
+            <View style={styles.item}>
+                <Text>{item.text}</Text>
+                <AntDesign name='delete' size={24} color='#000' onPress={() => onDeleteTodo(item.key)} />
+            </View>
         </TouchableOpacity>
     )
 }
 
 const styles = StyleSheet.create({
     item: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
         padding: 16,
         marginTop: 16,
         borderWidth: 1,
